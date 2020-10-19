d3a2f0f62f9e2a93f76c9c82410e5c848f67d86f
Ari Rostkier <roska87@gmail.com>
2020-10-19 04:26:49 -0300

    improvements

---
+++ app/src/main/java/mx/unam/petagram/Constants.java
@@ -0,0 +1,6 @@
+package mx.unam.petagram;
+
+public class Constants {
+    public static final String EMAIL = "test@gmail.com";
+    public static final String PASSWORD = "123456";
+}
--- app/src/main/java/mx/unam/petagram/activities/ContactActivity.java
+++ app/src/main/java/mx/unam/petagram/activities/ContactActivity.java
@@ -1,23 +1,17 @@
 package mx.unam.petagram.activities;
 
-import android.annotation.SuppressLint;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import org.apache.commons.lang3.StringUtils;
 
-import javax.mail.AuthenticationFailedException;
-import javax.mail.MessagingException;
-
+import mx.unam.petagram.Constants;
+import mx.unam.petagram.helpers.JavaMailAPI;
 import mx.unam.petagram.R;
-import mx.unam.petagram.helpers.Mail;
-
 
 public class ContactActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -59,60 +53,23 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
         int viewId = v.getId();
         switch (viewId) {
             case R.id.btnSendEmail:
-                EnviarEmail();
+                sendEmail();
                 break;
         }
     }
 
-    private void EnviarEmail() {
-        String[] recipients = { tiEmail.getText().toString() };
-        SendEmailAsyncTask email = new SendEmailAsyncTask();
-        email.activity = this;
-        String user = getResources().getString(R.string.user);
-        String password = getResources().getString(R.string.pasword);
-        email.m = new Mail(user, password);
-        email.m.set_from(user);
-        email.m.setBody(tiMessage.getText().toString());
-        email.m.set_to(recipients);
-        email.m.set_subject(tiName.getText().toString() + " - " +
-                tiSubject.getText().toString());
-        email.execute();
+    private void sendEmail() {
+        String toEmail = Constants.EMAIL;
+        String subject = tiSubject.getText().toString();
+        String message = this.formatMessage();
+        JavaMailAPI mail = new JavaMailAPI(this, toEmail, subject, message);
+        mail.execute();
     }
 
-    public void displayMessage(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    private String formatMessage(){
+        String contactEmail = tiEmail.getText().toString();
+        String message = tiMessage.getText().toString();
+        return StringUtils.join(contactEmail, "\n", message);
     }
 
-    @SuppressLint("StaticFieldLeak")
-    private class SendEmailAsyncTask extends AsyncTask<Void, Void, Boolean> {
-        Mail m;
-        ContactActivity activity;
-
-        public SendEmailAsyncTask() {
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            try {
-                m.send();
-                return true;
-            } catch (Exception e) {
-                activity.displayMessage(e.getMessage());
-                return true;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            if(aBoolean){
-                activity.displayMessage(getResources().getString(R.string.message_sent));
-                tiName.setText("");
-                tiEmail.setText("");
-                tiSubject.setText("");
-                tiMessage.setText("");
-            }else{
-                activity.displayMessage(getResources().getString(R.string.message_error));
-            }
-        }
-    }
 }
